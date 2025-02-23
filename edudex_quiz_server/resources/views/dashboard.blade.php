cf835c932faa047f2aaeddbbacbb7e1c3f29a325
copecute <copecute10x@gmail.com>
2025-02-23 22:37:19 +0700

    Docs: Sửa đổi usecase_tong_quat.drawio

--- edudex_quiz_server/resources/views/dashboard.blade.php
+++ edudex_quiz_server/resources/views/dashboard.blade.php
@@ -69,48 +69,6 @@
             <div class="card h-100">
                 <div class="card-body">
                     <h5 class="card-title">
-                        <i class="fas fa-book"></i> Quản lý môn học
-                    </h5>
-                    <p class="card-text">Thêm, sửa, xóa và quản lý môn học</p>
-                    <a href="{{ route('subjects.index') }}" class="btn btn-primary">
-                        <i class="fas fa-arrow-right"></i> Truy cập
-                    </a>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-4">
-            <div class="card h-100">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <i class="fas fa-question-circle"></i> Quản lý câu hỏi
-                    </h5>
-                    <p class="card-text">Thêm, sửa, xóa và quản lý ngân hàng câu hỏi</p>
-                    <a href="{{ route('questions.index') }}" class="btn btn-primary">
-                        <i class="fas fa-arrow-right"></i> Truy cập
-                    </a>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-4">
-            <div class="card h-100">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <i class="fas fa-file-alt"></i> Quản lý đề thi
-                    </h5>
-                    <p class="card-text">Tạo và quản lý các đề thi với phân bố câu hỏi theo tags</p>
-                    <a href="{{ route('test_papers.index') }}" class="btn btn-primary">
-                        <i class="fas fa-arrow-right"></i> Truy cập
-                    </a>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-4">
-            <div class="card h-100">
-                <div class="card-body">
-                    <h5 class="card-title">
                         <i class="fas fa-clipboard-list"></i> Quản lý kỳ thi
                     </h5>
                     <p class="card-text">Thêm, sửa, xóa và quản lý các kỳ thi</p>
